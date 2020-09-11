69a508b6b2da1d51e851c2b23a73945185c91a9a
Roman Shevtsov <roman.shevtsov@alumni.lcc.lt>
2020-09-11 16:45:32 +0300

    Updates

---
+++ precache-manifest.362c544dc98068b92a5a6314e8a0f026.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d3bed732104569d6448b2136bf14804",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e7ebd03738af9c057e05",
+    "url": "/monsters-rolodex/static/css/main.9e52a7ce.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7ebd03738af9c057e05",
+    "url": "/monsters-rolodex/static/js/main.b8a06d9b.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
